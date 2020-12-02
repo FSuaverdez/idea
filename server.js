cfb211640ddc311990097fc93ce708107c573662
FSuaverdez <sfrannz@gmail.com>
2020-12-02 19:49:53 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
+const { requireAuth, checkUser } = require('./middleware/authMiddleware')
+
+const app = express()
+
+// middleware
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cookieParser())
+// custome middleware
+app.use(authRoutes)
+
+// view engine
+app.set('view engine', 'ejs')
+
+// database connection
+const dbURI = 'CHANGE'
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
+
+// routes
+app.get('*', checkUser)
+app.get('/', (req, res) => res.redirect('login'))
